1f4c547c9bd600557ad55c97466ae7e15a31650a
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2022-12-15 20:09:39 +0200

    Update base script

--- base/base.sh
+++ base/base.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Set iptables-persistent
+sudo debconf-set-selections <<EOF
+iptables-persistent iptables-persistent/autosave_v4 boolean true
+iptables-persistent iptables-persistent/autosave_v6 boolean true
+EOF
+
+sudo dpkg --configure -a
+sudo apt-get install -f
+
+# Install tools for networking
+sudo apt-get install  -y dialog debconf-utils apt-utils iputils-ping iptables iputils-tracepath traceroute netcat conntrack nmap wget rsync iptables-persistent > /dev/null
+
 # Enable ipv4 forwarding
 sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 # Other ipsec settings
@@ -9,20 +21,14 @@ sudo echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
 
 sudo sysctl -p /etc/sysctl.conf
 
-# Install tools for networking
-sudo apt-get -y install dialog debconf-utils apt-utils iputils-ping iptables\
-  iputils-tracepath traceroute netcat conntrack nmap wget rsync
-
 # Install IPsec tools
-sudo apt-get -y install strongswan moreutils libstrongswan-extra-plugins
+sudo apt-get install -y strongswan moreutils libstrongswan-extra-plugins > /dev/null
 
-# Install iptables-persistent
-sudo debconf-set-selections <<EOF
-iptables-persistent iptables-persistent/autosave_v4 boolean true
-iptables-persistent iptables-persistent/autosave_v6 boolean true
-EOF
-sudo apt-get install -y iptables-persistent iptables-save
+
 
 # Install libs
-sudo apt-get install -y net-tools locate vim nano tcpdump dnsutils traceroute\
-  curl git-core bzip2 npm nodejs conntrack
+sudo apt-get install -y net-tools locate vim nano tcpdump dnsutils traceroute curl git-core bzip2 conntrack  > /dev/null
+
+# Add node16 repository
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+sudo apt-get install -y nodejs 
