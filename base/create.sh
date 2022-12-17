e8bb25c418d371cd15ed30ac61183904e82adbc1
Tuomas Aura <tuomas.aura@aalto.fi>
2022-12-18 00:30:22 +0200

    fix line endings

--- base/create.sh
+++ base/create.sh
@@ -3,7 +3,7 @@
 vagrant destroy -f base
 vagrant box remove base
 vagrant up base --provision
-vagrant vbguest base --status	
+vagrant vbguest base --status
 vagrant package --output ../base.box
 vagrant box add base ../base.box -f
 vagrant destroy -f base
